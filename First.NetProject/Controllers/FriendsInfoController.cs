4e03329af1efd1755feb3fe3e9013b173720e400
Hridoy Roy <roy.hridoy2014@gmail.com>
2019-10-22 11:41:48 +0600

    This is my first .net core project. Start my project

---
+++ First.NetProject/Controllers/FriendsInfoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First.NetProject.Controllers
+{
+    public class FriendsInfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
